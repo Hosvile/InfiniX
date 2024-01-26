bf26ef6534daec168765e7621e374dd02c64cfc4
Hosvile <100496294+Hosvile@users.noreply.github.com>
2024-01-26 22:00:30 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/Hosvile/InfiniX/releases/latest/download/main.lua",true))()￼Enter
+loadstring(game:HttpGet("https://github.com/Hosvile/InfiniX/releases/latest/download/main.lua", true))()
