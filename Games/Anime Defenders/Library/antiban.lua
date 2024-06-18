cb87d324ae8edff3607e0ca831356ec8728e3b52
Hosvile <100496294+Hosvile@users.noreply.github.com>
2024-06-18 15:45:08 +0800

    Update antiban.lua

--- Games/Anime Defenders/Library/antiban.lua
+++ Games/Anime Defenders/Library/antiban.lua
@@ -1,1 +1,31 @@
+local old; old = hookmetamethod(game, "__namecall", function(self, ...)
+	local method = string.lower(getnamecallmethod())
+	
+	if method == "fireserver" then
+		for i = 2, 4 do
+			local source, name = debug.info(i, "sn")
 
+			if source and source:match("Actions") and name == "Fire" then
+				local firstArg = ...
+
+				if select("#", ...) <= 0 then
+					game.GetService(game, "StarterGui").SetCore(game.GetService(game, "StarterGui"), "DevConsoleVisible", true)
+
+					warn("1 BAN INCOMMING 0 ARGS", debug.traceback())
+
+					return
+				elseif ... == game.GetService(game, "Players").LocalPlayer.UserId then
+					game.GetService(game, "StarterGui").SetCore(game.GetService(game, "StarterGui"), "DevConsoleVisible", true)
+
+					warn("1 BAN INCOMMING 0 ARGS USERID", debug.traceback())
+
+					return
+				end
+			end
+		end
+	end
+
+	RS_MT()
+	
+	return old(self, ...)
+end)
